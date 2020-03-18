0143ef81a2fc495870d4d509045f6473fb857a49
moacirtorres <moacirtorress@gmail.com>
2020-03-18 12:21:41 -0300

    JavaScript files

---
+++ js/principal.js
@@ -0,0 +1,22 @@
+var titulo = document.querySelector("h1")
+
+titulo.textContent = "Aparecida Nutricionista"
+
+
+var paciente = document.querySelector("#primeiro-paciente")
+
+var tdPeso = paciente.querySelector(".info-peso")
+var peso = tdPeso.textContent
+
+var tdAltura = paciente.querySelector(".info-altura")
+var altura = tdAltura.textContent
+
+
+
+var tdImc = paciente.querySelector(".info-imc")
+
+
+console.log(peso)
+console.log(altura)
+console.log(imc)
+
