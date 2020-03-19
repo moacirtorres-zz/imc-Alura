ccaccc7838ac844fc6e67014a8d847aae77004a2
moacirtorres <moacirtorress@gmail.com>
2020-03-19 11:36:12 -0300

    Validating blank spaces in form

--- js/form.js
+++ js/form.js
@@ -12,10 +12,9 @@ botaoAdicionar.addEventListener("click", function(event){
 
 
 	 var erros = validaPaciente(paciente)
-
+	 console.log(erros)
 	 if (erros.length > 0) {
-	 	var mensagemErro = document.querySelector("#mensagem-erro")
-	 	mensagemErro.textContent = erros
+	 	exibeMensagensDeErro(erros)
 	 	return
 	 }
 
@@ -77,11 +76,27 @@ function validaPaciente(paciente) {
 
 	var erros = []
 
-	if (!validaPeso(paciente.peso)) erros.push("Peso é inválido");
+	if(paciente.nome.length == 0) erros.push("O nome não pode ser em branco")
+	if(paciente.gordura.length == 0) erros.push("A gordura não pode ser em branco")
+	if(!validaPeso(paciente.peso)) erros.push("Peso é inválido");
 	if(!validaAltura(paciente.altura)) erros.push("Altura é inválida")
+	if(paciente.peso.length == 0) erros.push("O peso não pode ser em branco")
+	if(paciente.altura.length == 0) erros.push("A altura não pode ser em branco")
 
 
 	return erros;
 
 }
+
+
+function exibeMensagensDeErro(erros) {
+		var ul = document.querySelector("#mensagens-erro")
+		ul.innerHTML = ""
+	
+		erros.forEach(function(erro){
+			var li = document.createElement("li")
+			li.textContent = erro
+			ul.appendChild(li)
+		})
+	}
 
